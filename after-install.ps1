bfcc8862862ef279db414e308049d740069e7964
d2eason <drew.eason@gmail.com>
2018-09-30 13:19:24 -0500

    Add files via upload

---
+++ after-install.ps1
@@ -0,0 +1,8 @@
+﻿Import-Module -Name ServerManager
+$appPool = "DefaultAppPool"
+
+Write-Host "Starting $appPool"
+Start-WebAppPool -Name $appPool
+
+Write-Host "Resetting IIS"
+& iisreset /start
---
+++ before-install.ps1
@@ -0,0 +1,27 @@
+﻿Import-Module -Name ServerManager
+Install-WindowsFeature Web-Server
+
+$currentRetry = 0;
+$success = $false;
+$appPool = "DefaultAppPool"
+
+Write-Host "Stopping $appPool"
+Stop-WebAppPool -Name $appPool
+
+do{
+
+     $status = (Get-WebAppPoolState -name $appPool).Value
+    if ($status -eq "Stopped"){
+            $success = $true;
+            Write-Host "$appPool is $status."
+        }
+    else{
+        Write-Host "Let's wait a few seconds. $appPool is $status"
+        Start-Sleep -s 10
+        $currentRetry = $currentRetry + 1;
+        }
+    }
+while (!$success -and $currentRetry -le 4)
+
+WriteHost "Stop IIS"
+& iisreset /Stop
